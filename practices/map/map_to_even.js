6364aefaf68cc37f8a84db2af88a37b9db16c935
Wong Cho Hing <venus.wong@oocl.com>
2018-11-21 14:58:06 +0800

    map_to_even with map function

--- practices/map/map_to_even.js
+++ practices/map/map_to_even.js
@@ -1,6 +1,11 @@
 'use strict';
 
-function map_to_even(collection){
+function times2(element){
+  return element * 2;
+}
 
+function map_to_even(collection){
+  const map = collection.map(element => times2(element));
+  return map;
 }
 module.exports = map_to_even;
