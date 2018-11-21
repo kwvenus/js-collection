c21ce5e5fafdde4193d8c3cee292aea6136c6b67
Wong Cho Hing <venus.wong@oocl.com>
2018-11-21 14:49:51 +0800

    map to four multiples_add_one

--- practices/map/map_to_even.js
+++ practices/map/map_to_even.js
@@ -1,21 +1,6 @@
 'use strict';
 
-function plus1(element){
-  return element+1;
-}
-
-function times4(element){
-  return element*4;
-}
-
-function mappingElement(element){
-  return element => plus1(times4(element));
-}
-
 function map_to_even(collection){
 
-  const map = collection.map(mappingElement(element));
-
-  return map;
 }
 module.exports = map_to_even;
--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -1,6 +1,22 @@
 'use strict';
+
+function plus1(element){
+  return element+1;
+}
+
+function times4(element){
+  return element*4;
+}
+
+function mappingElement(element){
+  return plus1(times4(element));
+}
+
 var map_to_four_multiples_add_one = function(collection){
-  return [5,9,13,17,21];
+
+  const map = collection.map(element => mappingElement(element));
+
+  return map;
 };
 
 module.exports = map_to_four_multiples_add_one;
