89de8500b1f2c4107e986a7a9e40194014dfecb7
Wong Cho Hing <venus.wong@oocl.com>
2018-11-21 16:51:38 +0800

    grouping_count

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -2,7 +2,19 @@
 
 function grouping_count(collection) {
 
-  //implement here
+  let group_count = {};
+  let iteration;
+
+  for(iteration = 0; iteration < collection.length; iteration++){
+    if(collection[iteration] in group_count){
+      group_count[collection[iteration]] += 1;
+    }else{
+      group_count[collection[iteration]] = 1;
+    }
+  }
+
+  return group_count;
+
 }
 
 module.exports = grouping_count;
